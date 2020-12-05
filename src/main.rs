cd5500831e69a3034a5eac7468e137172c3e9f44
Xavier Schiller <22575255+XavierSchiller@users.noreply.github.com>
2020-12-05 14:13:38 +0530

    Zip::Reader|| Added Zip File Reader.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::path::Path;
+use std::fs::File;
 use walkdir::{DirEntry, WalkDir};
+use zip::ZipArchive;
 
-fn is_not_directory(entry: &DirEntry, path: &Path) -> bool {
-    if entry.path() == path {
-        return true;
-    } else {
-        return !entry.file_type().is_dir();
+fn is_zip_file(entry: &DirEntry) -> bool {
+    return entry.path().is_file() && entry.path().extension().unwrap() == "zip";
+}
+
+fn read_zip(path: &str) -> Vec<String> {
+    let zipfile = File::open(path).unwrap();
+    let mut res: Vec<String> = Vec::new();
+
+    let mut archive = ZipArchive::new(zipfile).unwrap();
+
+    for i in 0..archive.len() {
+        res.push(archive.by_index(i).unwrap().name().to_owned());
     }
+
+    res.sort_unstable();
+
+    return res;
 }
 
 fn main() {
-    let imp_path = Path::new("imported");
     WalkDir::new("imported")
+        .min_depth(1) // LOL I had to read the docs for this one.
+        .max_depth(1) // So that it doesn't recurse into the directories I don't want it to.
         .into_iter()
-        .filter_entry(|e| is_not_directory(e, imp_path))
+        .filter_entry(|e| is_zip_file(e))
         .filter_map(|v| v.ok())
-        .for_each(|x| println!("{}", x.path().display()))
+        .for_each(|x| println!("{:?}", read_zip(x.path().to_str().unwrap())));
+    std::process::exit(0);
 }
